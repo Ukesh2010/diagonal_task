fba5e4887c654dcbff1bb39a1588a9f8f654517c
ukesh gaiju <ukesh2010@gmail.com>
2019-07-20 14:21:45 +0545

    list and patch api created for task

--- app/src/Controller/TaskController.php
+++ app/src/Controller/TaskController.php
@@ -2,18 +2,46 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
+use App\Form\TaskType;
+use App\Repository\TaskRepository;
+use JMS\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/task")
+ */
 class TaskController extends AbstractController
 {
     /**
-     * @Route("/task", name="task")
+     * @Route("/", name="task_index", methods={"GET"})
      */
-    public function index()
+    public function index(TaskRepository $taskRepository, SerializerInterface $serializer): Response
     {
-        return $this->render('task/index.html.twig', [
-            'controller_name' => 'TaskController',
-        ]);
+        $data = $serializer->serialize($taskRepository->findAll(), 'json');
+
+        $jsonResponse = new JsonResponse(null, Response::HTTP_OK);
+        $jsonResponse->setJson($data);
+
+        return $jsonResponse;
+    }
+
+    /**
+     * @Route("/{taskId}/position-update", name="task_edit", methods={"PATCH"})
+     */
+    public function edit(Request $request, int $taskId): Response
+    {
+        $requestData = $request->getContent();
+
+        if (!isset($requestData['position'])) {
+            return new JsonResponse(['message' => 'position is required'], Response::HTTP_BAD_REQUEST);
+        }
+        
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 }
